b649970149b99d64f7c4ac5d9f8708f5994e0f02
aratimaharjan <aratimaharjan.3@gmail.com>
2021-04-19 20:43:54 +1000

    Update Program.cs

--- ScissorPaperRock/ScissorPaperRock/Program.cs
+++ ScissorPaperRock/ScissorPaperRock/Program.cs
@@ -7,6 +7,7 @@ namespace ScissorPaperRock
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi World!");
         }
     }
 }
